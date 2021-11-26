42459beaa60abfa2ac8486222a655b720ca1addc
FrankZiWANG-dev <wang.frank.zi@gmail.com>
2021-11-26 16:21:29 +0100

    ui almost done

--- AfterWork/src/App.vue
+++ AfterWork/src/App.vue
@@ -4,31 +4,81 @@
 </script>
 
 <template>
-  <div id='app'>
+  <div id='template'>
     <div id='title'>
-      <h1>
-        AfterWork
-      </h1>
-      <h2>
-        It's 5 o'clock somewhere
-      </h2>
+      <h1> AfterWork </h1>
+      <p id='slogan'> " It's 5 o'clock somewhere " </p>
+    </div>
+    <div id='content'>
+      <div id='generator-box'>
+        <div id='generator-title'>
+          <h2>What to drink...</h2>
+          <p>Click the button to get an idea!</p>
+        </div>
+        <div id='generator'>
+          <div id='generator-content'>
+            <img src='"https://github.com/FrankZiWANG-dev/AfterWork/blob/main/AfterWork/src/assets/images/drink-placeholder.jpg?raw=true"' alt='' id='drink'>
+            <p id='name'></p>
+            <p id='ingredients'></p>
+            <div>Inspire me!</div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <style>
 body{
-  background-image: url("https://github.com/FrankZiWANG-dev/AfterWork/blob/main/AfterWork/src/assets/images/bar.jpg?raw=true");
- background-position-x: 45%;
- background-position-y: 35%;
-  background-clip: content-box;
+  margin:0;
 }
 #app {
   font-family: 'Sora', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: white;
-  margin-top: 60px;
+  margin: 0;
+  background-color: #80190b;
 }
+#content{
+  background-image: url("https://github.com/FrankZiWANG-dev/AfterWork/blob/main/AfterWork/src/assets/images/bar-table.jpg?raw=true");
+  background-size:cover;
+  background-position-x: 70%;
+  background-position-y: 20%;
+  height: 100vh;
+}
+#title{
+  padding-top: 10px;
+  text-shadow: 2px 2px #1E1E1E;
+}
+  h1{
+    margin: 10px 0px 0px 35px;
+  }
+  #slogan{
+    font-size: 0.9em;
+    margin-top: 2px;
+    margin-left: 35px;
+    color: #dbd6d6;
+  }
+#generator-box{
+  padding: 20px;
+  text-align: center;  
+}
+  #generator-title{
+    text-shadow: 2px 2px #1E1E1E;
+  }
+  #generator{
+    background-color: white;
+    opacity: 0.7;
+    
+    margin-left: auto;
+    margin-right: auto;
+    height: 300px;
+    width: 300px;
+    border-radius: 10px;  
+  }
+  #generator-content{
+    color: #1E1E1E;
+    opacity: 1;
+  }
 </style>
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # AfterWork
+## Images
+- Bar table image by Pixabay at https://www.pexels.com/fr-fr/photo/bouteille-de-ballantine-avec-deux-verres-rock-301692/
+- Alcohol bottles image by Isabella Mendes at https://www.pexels.com/fr-fr/photo/quatre-bouteilles-d-alcool-assorties-340996/
