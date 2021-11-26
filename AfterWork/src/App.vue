bfcdb36f71b2824223d6dd11b6ac605c2790de48
FrankZiWANG-dev <wang.frank.zi@gmail.com>
2021-11-26 15:16:53 +0100

    new image

--- AfterWork/src/App.vue
+++ AfterWork/src/App.vue
@@ -1,33 +1,34 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
- <main>
-    <div>
-        <h1>AfterWork</h1>
-        <p>It's 5 0'clock somewhere</p>
+  <div id='app'>
+    <div id='title'>
+      <h1>
+        AfterWork
+      </h1>
+      <h2>
+        It's 5 o'clock somewhere
+      </h2>
     </div>
-    <div>
-        <p>
-          Drink
-        </p>
-    </div>
-</main>
+  </div>
 </template>
 
 <style>
+body{
+  background-image: url("https://github.com/FrankZiWANG-dev/AfterWork/blob/main/AfterWork/src/assets/images/bar.jpg?raw=true");
+ background-position-x: 45%;
+ background-position-y: 35%;
+  background-clip: content-box;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Sora', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: white;
   margin-top: 60px;
-}
-main{
-   background-image: url("/assets/images/bar.jpg");  
 }
 </style>
