a1d81a68feb3f3e988f65f2b26675d9e2b3f2519
FrankZiWANG-dev <wang.frank.zi@gmail.com>
2021-11-28 18:26:41 +0100

    revamp

---
+++ components/intro.js
@@ -0,0 +1,6 @@
+<div id='intro'>
+    <p>Long day at work?<br/>
+    Don't know what to get?</p>
+    <p>Click the button to get an idea!</p>
+</div>
+
---
+++ main.js
@@ -0,0 +1,24 @@
+const app = Vue.createApp({
+    data(){
+    }
+})
+// import { createApp } from 'vue'
+// import App from './App.vue'
+
+// const response = await fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php");
+// const data = await response.json();
+// const drink = data.drinks[0];
+// const drinkName = drink.strDrink;
+// const ingredients = [];
+// for (let x=1;x<16;x++){
+//     let ingredientName = "strIngredient"+x;
+//     let measure = "strMeasure"+x;
+//     if (drink[measure] !== null){
+//         let ingredient = String(drink[measure] + " " + drink[ingredientName]);
+//         ingredients.push(ingredient);
+//     }
+// }
+// console.log(drink);
+// console.log(ingredients);
+
+// createApp(App).mount('#app');
